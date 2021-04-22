20082ddacdbb44cf71b97e36cd50589f84f43706
Norberto Hideaki Enomoto <norberto.enomoto@gmail.com>
2021-04-21 22:08:31 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-boot-docker-compose
 Using Spring Boot Restful API + Docker Compose
+
+https://springframework.guru/manage-docker-containers-with-docker-compose/
+https://github.com/spring-framework-guru/sfg-blog-posts/tree/master/spring-boot-docker-compose
+
---
+++ user-service/src/main/java/guru/springframework/dockercompose/controller/UserController.java
@@ -0,0 +1,47 @@
+package guru.springframework.dockercompose.controller;
+
+import guru.springframework.dockercompose.domain.User;
+import guru.springframework.dockercompose.exception.UserAlreadyExistsException;
+import guru.springframework.dockercompose.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * RestController annotation is used to create
+ * Restful web services using Spring MVC
+ */
+@RestController
+
+/**
+ * RequestMapping annotation maps
+ * HTTP requests to handler methods
+ */
+@RequestMapping(value = "/api/v1/users")
+public class UserController {
+    private UserService userService;
+    ResponseEntity responseEntity;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * PostMapping Annotation for mapping HTTP POST requests onto
+     * specific handler methods.
+     */
+    @PostMapping
+    public ResponseEntity<?> saveUser(@RequestBody User user) throws UserAlreadyExistsException, Exception {
+        User savedUser = userService.saveUser(user);
+        responseEntity = new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+        return responseEntity;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<User>> getUsers() {
+        Iterable<User> users = userService.getUsers();
+        return ResponseEntity.ok(users);
+    }
+}
